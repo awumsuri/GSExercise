0f7168c019b3984947109d2f6ffe3ecc54139aed
Suri <awumsuri@gmail.com>
2018-08-02 10:42:29 -0400

    Stable

--- src/app/components/container/assessment/assessment.js
+++ src/app/components/container/assessment/assessment.js
@@ -2,14 +2,14 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { HeaderView } from '../../presentation/assessment/Views'
 
-class Exam extends PureComponent{
+class Assessment extends PureComponent {
     constructor(props) {
         super()
     }
 
     render() {
        return (
-            <HeaderView title={"Welcome " + this.props.username} />
+           <div> <HeaderView title={"welcome " + this.props.username} /></div>
        )
     }
 }
@@ -17,4 +17,4 @@ class Exam extends PureComponent{
 const mapStateToProps = state => ({state})
 const mapDispatchToProps = dispatch => ({dispatch})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Exam)
+export default connect(mapStateToProps, mapDispatchToProps)(Assessment)
--- src/app/components/presentation/assessment/Views.js
+++ src/app/components/presentation/assessment/Views.js
@@ -6,3 +6,24 @@ export const HeaderView = (props) => (
     </div>
 )
 
+export const BiQuestionView = (props) => (
+    <div>
+        <div><span>{props.heading}</span></div>
+        <div><span><p>{props.question}</p></span></div>
+        <div>
+            <button name="yes" onClick={this.props.onClick}> Yes </button>
+            <button name="no" onClick={this.props.onClick}> No </button>
+        </div>
+    </div>
+)
+
+export const MultiQuestionView = (props) => (
+    <div>
+        <div><span>{props.heading}</span></div>
+        <div><span><p>{props.question}</p></span></div>
+        <ul>
+            {props.questions.map(question => (<li>{question}</li>))}
+        </ul>
+    </div>   
+)
+
