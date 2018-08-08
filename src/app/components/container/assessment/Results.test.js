fae29b83036e6f94219c212478fba70326a9e16e
Suri <awumsuri@gmail.com>
2018-08-07 20:23:15 -0400

    Added Tests

--- src/app/components/container/assessment/Results.test.js
+++ src/app/components/container/assessment/Results.test.js
@@ -7,8 +7,9 @@ import questions from '../../../model/assessment/questions'
 import allAnswers from '../../../model/assessment/mocks/allAnswers'
 import answeredObject from '../../../model/assessment/mocks/answeredObject'
 
-const answers = [ 1,4,42,40,43,189 ]
 Enzyme.configure({ adapter: new Adapter() });
+
+const answers = [ 1,4,42,40,43,189 ]
 const setup = () => {
     const props = {
         questions: questions,
@@ -36,9 +37,21 @@ describe("Results Unit Test", () => {
     })
 
     it("should calculate answeredObject correctly", () => {
-        const calculatedAnswerObject =  result.addQuestionNumberToAnswered(questions, 
-            result.mapQuestionsToAnswered(allAnswers, answers))
+        const calculatedAnswerObject =  
+            result.addQuestionNumberToAnswered(questions, result.mapQuestionsToAnswered(allAnswers, answers))
 
         expect(calculatedAnswerObject).toEqual(answeredObject)
+    })
+
+    it("should calculate total possible score correctly", () => {
+        const TOTAL_POSSIBLE = 18
+
+        expect(result.getTotalPossibleScore(allAnswers)).toEqual(TOTAL_POSSIBLE)
+    })
+
+    it("should calculate total score correctly", () => {
+        const ANSWERED_SCORE = 8
+
+        expect(result.getTotalScore(answeredObject)).toEqual(ANSWERED_SCORE)
     })
 })
