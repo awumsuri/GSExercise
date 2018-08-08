efa36e8822436268eda862784561b084ad24dd10
Suri <awumsuri@gmail.com>
2018-08-07 20:15:04 -0400

    Added test

--- src/app/components/container/assessment/Results.test.js
+++ src/app/components/container/assessment/Results.test.js
@@ -4,12 +4,15 @@ import Adapter from 'enzyme-adapter-react-16'
 import expect from 'expect'
 import Results from './Results'
 import questions from '../../../model/assessment/questions'
+import allAnswers from '../../../model/assessment/mocks/allAnswers'
+import answeredObject from '../../../model/assessment/mocks/answeredObject'
 
+const answers = [ 1,4,42,40,43,189 ]
 Enzyme.configure({ adapter: new Adapter() });
 const setup = () => {
     const props = {
         questions: questions,
-        answers: [ 1,0,3,4,5,61 ] 
+        answers: answers 
     }
     const wrapper = mount(<Results { ...props } />)
 
@@ -19,10 +22,23 @@ const setup = () => {
     }
 }
 
-describe("Results API Test", () => {
-    it("should calculate reducers correctly", () => {
-        const { wrapper } = setup()
-        console.log("wrapper:", wrapper)
+const { wrapper } = setup()
+const result = new Results(wrapper.props())
+
+describe("Results Unit Test", () => {
+    it("props passed through correctly", () => {
         expect(wrapper.props().questions).toEqual(questions)
+        expect(wrapper.props().answers).toEqual(answers)
+    })
+
+    it("should calculate getAllAnswers correctly", () => {
+        expect(result.getAllAnswers(questions)).toEqual(allAnswers)
+    })
+
+    it("should calculate answeredObject correctly", () => {
+        const calculatedAnswerObject =  result.addQuestionNumberToAnswered(questions, 
+            result.mapQuestionsToAnswered(allAnswers, answers))
+
+        expect(calculatedAnswerObject).toEqual(answeredObject)
     })
 })
